3ddea34098e46fb04d378bd05d77a2f3826412f6
Yibei Chen <yibeichan@gmail.com>
2023-12-20 20:53:57 +0000

    set up protocol schema structure autoupdate

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -2,6 +2,40 @@ import json
 import os
 import shutil
 
+def get_pref_label(activity_path):
+    try:
+        with open(activity_path, 'r') as file:
+            activity_schema = json.load(file)
+        return activity_schema.get("prefLabel", "Unknown Activity")
+    except (FileNotFoundError, json.JSONDecodeError):
+        return "Unknown Activity"
+
+def update_json_schema(activities, base_path):
+    schema_file = os.path.join(base_path, '{{ cookiecutter.__protocol_slug }}/{{ cookiecutter.__protocol_slug }}_schema')  # Update with the correct relative path
+    with open(schema_file, 'r') as file:
+        schema = json.load(file)
+
+    # Clear existing activities in ui and order
+    schema['ui']['addProperties'] = []
+    schema['ui']['order'] = []
+
+    # Update ui and order based on available activities
+    for activity in activities:
+        activity_schema_path = os.path.join(base_path, f'activities/{activity}/{activity}_schema')
+        pref_label = get_pref_label(activity_schema_path)
+        activity_path = f"../activities/{activity}/{activity}_schema"
+
+        schema['ui']['addProperties'].append({
+            "isAbout": activity_path,
+            "variableName": f"{activity}_schema",
+            "prefLabel": pref_label
+        })
+        schema['ui']['order'].append(activity_path)
+
+    # Save the updated schema
+    with open(schema_file, 'w') as file:
+        json.dump(schema, file, indent=4)
+
 def main():
     base_path = os.getcwd()  # Base path of the generated project
     activities_path = os.path.join(base_path, 'activities')
@@ -27,6 +61,10 @@ def main():
             activity_dir = os.path.join(activities_path, activity)
             if os.path.exists(activity_dir):
                 shutil.rmtree(activity_dir)
+
+    # Update the JSON schema
+    activities = [activity for activity in selected_activities if os.path.exists(os.path.join(activities_path, activity))]
+    update_json_schema(activities, base_path)
 
 if __name__ == "__main__":
     main()
