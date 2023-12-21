4129aa9c7e3166f21cb7be02b311a5bd2e41a989
Yibei Chen <yibeichan@gmail.com>
2023-12-21 16:45:35 +0000

    adding missing quotation

--- {{cookiecutter.protocol_name}}/ui-changes/src/config.js
+++ {{cookiecutter.protocol_name}}/ui-changes/src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   /* eslint-disable */
   githubSrc: 'https://raw.githubusercontent.com/{{cookiecutter.github_org}}/{{cookiecutter.protocol_name}}/main/{{cookiecutter.__protocol_slug}}/{{cookiecutter.__protocol_slug}}_schema',
-  banner: {{cookiecutter.protocol_description}},
+  banner: '{{cookiecutter.protocol_description}}',
   startButton: 'Join',
   assetsPublicPath: '/{{cookiecutter.__protocol_slug}}/',
   backendServer: null
